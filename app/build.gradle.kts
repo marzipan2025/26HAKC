60c52657371edbe060fddef8b5d586a904760c64
Agni <artbrainbyeongsu@gmail.com>
2026-08-24 23:09:41 +0900

    0.2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.artbrain.hakc"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "0.2.6.3"
+        versionCode = 47
+        versionName = "0.2.7"
     }
 
     signingConfigs {
